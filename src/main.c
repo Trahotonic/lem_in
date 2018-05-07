7571f13eb9de7c04501a4305c86ce36f3088dc03
Roman Kyslyy <rkyslyy@Romans-MacBook-Pro.local>
2018-05-07 10:37:45 +0300

    ants_moving

--- src/main.c
+++ src/main.c
@@ -17,14 +17,14 @@ int main(void)
 	paths = ft_init_path(start);
 	while (ft_open_paths(paths))
 		ft_get_paths(paths, start);
-	ft_print_paths(paths);
+//	ft_print_paths(paths);
 //	if (ft_successful_roads(paths))
 //		ft_print_path(ft_get_shortest_path(paths));
 //	else
 //		ft_printf("All paths lead to dead end\n");
 	ft_get_ants(ants_q, &ants, paths);
-	ft_print_ants(ants);
+//	ft_print_ants(ants);
     ft_move_ants(ants, start);
-//	system("leaks lem-in");
+    sleep(50);
 	return 0;
 }
